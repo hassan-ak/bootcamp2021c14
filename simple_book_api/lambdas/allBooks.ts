336827104660cd9f657b9250d494f19437830a49
Hassan Ali Khan <h.a.khan1992fl@gmail.com>
2021-12-09 01:00:34 +0500

    Update allBooks.ts

--- simple_book_api/lambdas/allBooks.ts
+++ simple_book_api/lambdas/allBooks.ts
@@ -10,6 +10,34 @@ export const handler = async (event: any, context: any): Promise<any> => {
   };
   try {
     const response = await db.scan(params).promise();
+
+    // When there is limit in the query
+    if (
+      event.queryStringParameters &&
+      event.queryStringParameters.limit &&
+      !event.queryStringParameters.type
+    ) {
+      if (!parseInt(event.queryStringParameters.limit)) {
+        return {
+          statusCode: 200,
+          body: `{ "message": "Enter Limit in numeric form" }`,
+        };
+      }
+      if (parseInt(event.queryStringParameters.limit)) {
+        if (!response.Items) {
+          return {
+            statusCode: 200,
+            body: `{ "message": "No Books currently available" }`,
+          };
+        } else {
+          const result = response.Items.slice(
+            0,
+            Math.abs(event.queryStringParameters.limit)
+          );
+          return { statusCode: 200, body: JSON.stringify(result) };
+        }
+      }
+    }
     return { statusCode: 200, body: JSON.stringify(response.Items) };
   } catch (err) {
     console.log("DynamoDB error: ", err);
