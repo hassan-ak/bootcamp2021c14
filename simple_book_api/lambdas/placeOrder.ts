7ab5dd73250df543a098f9390d049febe795f684
Hassan Ali Khan <h.a.khan1992fl@gmail.com>
2021-12-10 05:58:24 +0500

    Update placeOrder.ts

--- simple_book_api/lambdas/placeOrder.ts
+++ simple_book_api/lambdas/placeOrder.ts
@@ -2,6 +2,8 @@ import * as AWS from "aws-sdk";
 const db = new AWS.DynamoDB.DocumentClient();
 
 const TABLE_NAME_USER = process.env.TABLE_NAME_USER || "";
+const TABLE_NAME_ALL = process.env.TABLE_NAME_ALL || "";
+const PRIMARY_KEY_ALL = process.env.PRIMARY_KEY_ALL || "";
 
 export async function handler(event: any) {
   if (!event.headers.Authorization) {
@@ -78,7 +80,26 @@ export async function handler(event: any) {
           );
           item["userName"] = userResponse[0].userName;
           item["userEmail"] = userResponse[0].userEmail;
+          const params2 = {
+            TableName: TABLE_NAME_ALL,
+            Key: {
+              [PRIMARY_KEY_ALL]: item.bookID,
+            },
+          };
+          const response2 = await db.get(params2).promise();
+          if (!response2.Item) {
+            return {
+              statusCode: 400,
+              body: `{ "Error": "No book with requested ID - Try Again" }`,
+            };
+          }
+          item["book_type"] = response2.Item.book_type;
+          item["book"] = response2.Item.book;
+          item["isbn"] = response2.Item.isbn;
+          item["price"] = response2.Item.price;
+          item["author"] = response2.Item.author;
           console.log(item);
+          console.log(item.isbn);
           return {
             statusCode: 200,
             body: `{ "status": "OK from place Order" }`,
