d45c063862e29e626a69bfe016325363985dbc23
Hassan Ali Khan <h.a.khan1992fl@gmail.com>
2021-12-10 05:12:32 +0500

    Update placeOrder.ts

--- simple_book_api/lambdas/placeOrder.ts
+++ simple_book_api/lambdas/placeOrder.ts
@@ -1,3 +1,8 @@
+import * as AWS from "aws-sdk";
+const db = new AWS.DynamoDB.DocumentClient();
+
+const TABLE_NAME_USER = process.env.TABLE_NAME_USER || "";
+
 export async function handler(event: any) {
   if (!event.headers.Authorization) {
     return {
@@ -46,18 +51,48 @@ export async function handler(event: any) {
   }
   item["user_ID"] = event.headers.Authorization.split(" ")[1];
 
-  ///////
-  ///////
-  ///////
-  ///////
-  ///////
+  const params1 = {
+    TableName: TABLE_NAME_USER,
+  };
   try {
-    console.log(item);
-    return {
-      statusCode: 200,
-      body: `{ "status": "OK from place Order" }`,
-    };
+    const response = await db.scan(params1).promise();
+    if (response.Count === 0) {
+      return {
+        statusCode: 400,
+        body: `{ "message": "You are not a registered user. Register Yourself or provide correct user Key" }`,
+      };
+    } else {
+      if (response.Items) {
+        if (
+          response.Items.filter(
+            (userItem) => userItem.user_ID === item["user_ID"]
+          ).length === 0
+        ) {
+          return {
+            statusCode: 400,
+            body: `{ "message": "You are not a registered user. Register Yourself or provide correct user Key" }`,
+          };
+        } else {
+          const userResponse = response.Items.filter(
+            (userItem) => userItem.user_ID === item["user_ID"]
+          );
+          item["userName"] = userResponse[0].userName;
+          item["userEmail"] = userResponse[0].userEmail;
+          console.log(item);
+          return {
+            statusCode: 200,
+            body: `{ "status": "OK from place Order" }`,
+          };
+        }
+      } else {
+        return {
+          statusCode: 400,
+          body: `{ "message": "You are not a registered user. Register Yourself or provide correct user Key" }`,
+        };
+      }
+    }
   } catch (error) {
+    console.log("Error = ", error);
     return {
       statusCode: 500,
       body: error,
